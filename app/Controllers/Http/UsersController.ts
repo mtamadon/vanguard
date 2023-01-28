345be62777a6274e19d3ea636ef9818f955c6caf
Mahdi Tamadon <mahdi.tamadon@snapp.cab>
2023-01-28 18:02:46 +0100

    verify mobile when updating it

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,6 +1,7 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 import User from "App/Models/User"
+import UserSession from 'App/Models/UserSession'
 
 export default class UsersController {
 
@@ -12,9 +13,18 @@ export default class UsersController {
 
     public async profileUpdate({ request, response, userId }: HttpContextContract) {
         const user = await User.findOrFail(userId)
+        const oldPhoneNumber = user.phoneNumber
         console.log(request.all())
         user.merge(request.only(['name', 'email', 'phone_number', 'city', 'country', 'language']))
+        if (oldPhoneNumber !== user.phoneNumber) {
+            user.status = 2 // 2 = phone number not verified            
+        }
         await user.save()
+
+        if (user.status === 2) {
+            // logout all sessions
+            await UserSession.query().where('user_id', user.id).delete()
+        }
         return response.json({ user })
     }
 
